076f39e791766c862b46190fc7548737861df4df
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2024-11-05 20:23:36 +0530

    Project Init

---
+++ src/test/java/com/lldpractice/LowLevelDesign/LowLevelDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lldpractice.LowLevelDesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowLevelDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
